00f5780b526b2893150e8380cb66a564031673ca
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-06-05 17:42:20 +0530

    Updates

---
+++ precache-manifest.e6697178721883da78f17e6afe477d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd7c6faf8e8a935bd739879a5a4db2a",
+    "url": "/SecurityLit/index.html"
+  },
+  {
+    "revision": "34edbfbc325d40c2e82b",
+    "url": "/SecurityLit/static/css/main.8cf64d27.chunk.css"
+  },
+  {
+    "revision": "2f9a10868cc92357aa1a",
+    "url": "/SecurityLit/static/js/2.c5fe178e.chunk.js"
+  },
+  {
+    "revision": "6ccc81b7c7469e832ffd17a54adf1ad2",
+    "url": "/SecurityLit/static/js/2.c5fe178e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34edbfbc325d40c2e82b",
+    "url": "/SecurityLit/static/js/main.ed1a835c.chunk.js"
+  },
+  {
+    "revision": "34873977b2b55b3d0898",
+    "url": "/SecurityLit/static/js/runtime-main.9d034e32.js"
+  }
+]);
