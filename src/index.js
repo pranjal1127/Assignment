41c5b94442db19a1fb1024d824fbee77b69f0620
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-06-05 17:46:47 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {  BrowserRouter as Router,  Switch, Route } from "react-router-dom";
+import {  hashRouter as Router,  Switch, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import DataTable from './DataTable';
